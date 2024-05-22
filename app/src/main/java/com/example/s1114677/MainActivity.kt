ccd6606fcfe362d1af94c78d42c275f228a954c1
LLLTZUYIN <b203120708@gmail.com>
2024-05-22 15:39:08 +0800

    v1_林姿穎

--- app/src/main/java/com/example/s1114677/MainActivity.kt
+++ app/src/main/java/com/example/s1114677/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             S1114677Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Androi")
                 }
             }
         }
